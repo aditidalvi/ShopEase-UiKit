5a2c5a00c76916dd6835e2f5173a7672494b2ff9
aditidalvi <aditidalvi22@gmail.com>
2025-05-03 17:27:15 +0530

    created a xcode project and initial setup

---
+++ ShopEase-Uikit/ShopEase-Uikit/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ShopEase-Uikit
+//
+//  Created by Aditi Dalvi on 03/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemRed
+    }
+}
+
