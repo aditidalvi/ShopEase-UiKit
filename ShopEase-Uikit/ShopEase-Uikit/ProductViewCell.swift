df2be871ba32a6f410db55ecf24632ddae0a2f89
aditidalvi <aditidalvi22@gmail.com>
2025-05-04 21:26:21 +0530

    separated the logic in api fucntion to make it more readable

---
+++ ShopEase-Uikit/ShopEase-Uikit/ProductViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProductViewCellCollectionViewCell.swift
+//  ShopEase-Uikit
+//
+//  Created by Aditi Dalvi on 04/05/25.
+//
+
+import UIKit
+
+class ProductViewCell: UICollectionViewCell {
+    
+}
