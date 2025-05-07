bf448a53d5bf4cb6a831c73ac8c42d44b484c421
aditidalvi <aditidalvi22@gmail.com>
2025-05-07 08:00:10 +0530

    logic separation

--- ShopEase-Uikit/ShopEase-Uikit/HomeScreenViewModel.swift
+++ ShopEase-Uikit/ShopEase-Uikit/HomeScreenViewModel.swift
@@ -20,26 +20,30 @@ class HomeScreenViewModel {
                   response.statusCode == 200 else {
                 return
             }
-            guard var products = try? JSONDecoder().decode([ProductModel].self, from: data) else {
+            guard let products = try? JSONDecoder().decode([ProductModel].self, from: data) else {
                 assertionFailure("Unable to decode the data")
                 return
             }
-            // Assuming ProductVM is: ProductVM(category: String, products: [ProductData])
-            var categorizedProducts: [String: [ProductData]] = [:]
-            for product in products {
-                let productData = ProductData(image: product.image, title: product.title, price: product.price)
-                categorizedProducts[product.category, default: []].append(productData)
-            }
-            // Now map the dictionary into an array of ProductVM
-            let productVMs: [ProductVM] = categorizedProducts.map { category, products in
-                ProductVM(category: category, products: products)
-            }
-            self?.products = productVMs.sorted(by: {
-                $0.category > $1.category
-            })
+            self?.updateProduct(products: products)
             DispatchQueue.main.async {
                 completion()
             }
         }).resume()
+    }
+    
+    private func updateProduct(products: [ProductModel]) {
+        // Assuming ProductVM is: ProductVM(category: String, products: [ProductData])
+        var categorizedProducts: [String: [ProductData]] = [:]
+        for product in products {
+            let productData = ProductData(image: product.image, title: product.title, price: product.price)
+            categorizedProducts[product.category, default: []].append(productData)
+        }
+        // Now map the dictionary into an array of ProductVM
+        let productVMs: [ProductVM] = categorizedProducts.map { category, products in
+            ProductVM(category: category, products: products)
+        }
+        self.products = productVMs.sorted(by: {
+            $0.category > $1.category
+        })
     }
 }
